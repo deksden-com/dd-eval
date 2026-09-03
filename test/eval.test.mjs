3715c8aa8e2ba94fd61d1862dd035b7ac574abc4
Denis Kiselev <deksden@deksden.com>
2026-09-03 16:08:10 +0200

    chore(eval): pin AGY e2e to flow beta.7

--- test/eval.test.mjs
+++ test/eval.test.mjs
@@ -21,10 +21,10 @@ test("case pins its input checkpoint and exact engine without Session starter st
   assert.equal("starter_sessions" in loaded.value, false);
   assert.equal("canonical_checkpoints" in loaded.value, false);
   assert.equal("priming" in loaded.value, false);
-  assert.equal(loaded.inputCheckpoint.value.id, "cp-060-task-priority-flow-4-0-1-engine-0-9-0-beta-6");
+  assert.equal(loaded.inputCheckpoint.value.id, "cp-061-task-priority-flow-4-0-1-engine-0-9-0-beta-7");
   assert.equal(loaded.inputCheckpoint.value.source.commit, "86db34668add2cd2f9a7c59adfced5e7ae57c3b8");
   assert.equal(loaded.inputCheckpoint.value.flow_pack.commit, "c01b8ce397cd07aeaf055efe4ca58ab940f4d519");
-  assert.equal(loaded.inputCheckpoint.value.flow_pack.engine.version, "0.9.0-beta.6");
+  assert.equal(loaded.inputCheckpoint.value.flow_pack.engine.version, "0.9.0-beta.7");
   assert.deepEqual(loaded.value.flow.contour, ["specify", "protocolize", "plan", "plan-review", "code", "code-review", "merge"]);
 });
 
